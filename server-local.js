12366213fb1d503f18cf23e7acccf1c208285985
Vikramaditya <vk92kokil@live.com>
2019-07-29 19:10:07 +0530

    add airbrake monitoring

--- server-local.js
+++ server-local.js
@@ -1,4 +1,5 @@
 'use strict';
+var AirbrakeClient = require('airbrake-js');
 var server = require('./server/proxy');
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
@@ -8,3 +9,10 @@ var port = process.env.PORT || 8080;
 server.listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
+if(process.env.NODE_ENV === 'production') {
+  var airbrake = new AirbrakeClient({
+    projectId: process.env.AIRBRAKE_PROJECT_ID,
+    projectKey: process.env.AIRBRAKE_API_KEY
+  });
+}
